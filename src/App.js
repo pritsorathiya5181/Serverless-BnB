c862711c010c24bd5039723cecc4ed4f27b407cd
ngulati <nl429326@dal.ca>
2022-07-05 16:22:32 -0300

    adding online support changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import './App.css'
+import Chatbot from './components/ChatBot'
+import Orders from './components/Orders'
 import LoginPage from './pages/authentication/login/LoginPage'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/chatbot' element={<Chatbot />} />
+        <Route path='/orders' element={<Orders />} />
       </Routes>
     </Router>
   )
