c4fb9a8c496da8848566f4aed2d367db6618405d
ngulati <nl429326@dal.ca>
2022-07-05 16:40:31 -0300

    adding lambda code for online support

---
+++ online-support-lambda/roomReactApi.js
@@ -0,0 +1,61 @@
+const AWS = require("aws-sdk");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+// code references : https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-dynamo-db.html, https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/dynamodb-example-query-scan.html
+
+AWS.config.update({ region: "us-east-1" });
+
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+    switch (event.routeKey) {
+      case "GET /rooms/{userId}":
+          
+        const params = {
+             FilterExpression: "userId = :u",
+            ExpressionAttributeValues: {
+            ":u": {S: event.pathParameters.userId}
+            },
+            ExpressionAttributeNames: {
+            '#n': 'number',
+            '#t': 'type',
+            },
+            ProjectionExpression: "cost,#n,#t",
+            TableName: "Room",
+        };  
+        
+        body = await ddb.scan(params, function (err, data) {
+        if (err) {
+          console.log("Error", err);
+        } else {
+            console.log("Success", data.Count);
+        }
+      }).promise();
+        break;
+      case "GET /rooms":
+        body = await dynamo.scan({ TableName: "Room" }).promise();
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
+};
