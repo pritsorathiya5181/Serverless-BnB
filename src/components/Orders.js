59e48f6828030fc601ba942266a673997864ccc9
Prit Ajaykumar Sorathiya <pr669751@dal.ca>
2022-07-17 20:21:20 -0300

    Add deployment setup code

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,94 +1,139 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import  Chatbot  from "../components/ChatBot";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Chatbot from '../components/ChatBot'
+import NotFound from '../pages/404'
 
 const Orders = (props) => {
-    const [rooms, setRooms] = useState({});
-    const [food, setFood] = useState({});
+  const [rooms, setRooms] = useState({})
+  const [food, setFood] = useState({})
 
-    const fetchDetails = async () => {
-        // let userId ='82178782112'
-        let userId=localStorage.getItem('userId')
-        const r = await axios.get('https://iwna4wu91b.execute-api.us-east-1.amazonaws.com/rooms/' + userId).catch((err) => {
-            console.log(err)
-        })
+  const fetchDetails = async () => {
+    // let userId ='82178782112'
+    let userId = localStorage.getItem('userId')
+    const r = await axios
+      .get(
+        'https://iwna4wu91b.execute-api.us-east-1.amazonaws.com/rooms/' + userId
+      )
+      .catch((err) => {
+        console.log(err)
+      })
 
-        setRooms(r.data)
+    setRooms(r.data)
 
-        // let id = '7217721781221'
-        let id=localStorage.getItem('userId')
-        const r2 = await axios.get('https://iwna4wu91b.execute-api.us-east-1.amazonaws.com/kitchen/' + id).catch((err) => {
-            console.log(err)
-        })
+    // let id = '7217721781221'
+    let id = localStorage.getItem('userId')
+    const r2 = await axios
+      .get(
+        'https://iwna4wu91b.execute-api.us-east-1.amazonaws.com/kitchen/' + id
+      )
+      .catch((err) => {
+        console.log(err)
+      })
 
-        // console.log(userId)
-        setFood(r2.data)
-    }
+    // console.log(userId)
+    setFood(r2.data)
+  }
 
-    useEffect(() => {
-        fetchDetails();
+  useEffect(() => {
+    fetchDetails()
 
-        console.log(food)
-        console.log(rooms)
-    }, []);
+    console.log(food)
+    console.log(rooms)
+  }, [])
 
+  if (props.auth.user) {
     return (
-        <div className='container' >
-            <div className="card container" style={{ width: 18 + 'rem', top: 5 + 'rem', position: 'relative', right: 420 + 'px' }}>
-                <div className="card-body">
-                    <h5 className="card-title">ROOMS BOOKED : {rooms.Count} </h5>
-                    <img className="card-img-top" src="https://source.unsplash.com/1x1?hotel,rooms" alt="Card image cap" />
-                    {rooms.Items !== undefined && rooms.Items.length>0 ? (
-                        <>
-                        <p className="card-title">NUMBER : <nbsp />
-                            {rooms.Items[rooms.Count-1].number.S} , ...
-                        </p>
+      <div className='container'>
+        <div
+          className='card container'
+          style={{
+            width: 18 + 'rem',
+            top: 5 + 'rem',
+            position: 'relative',
+            right: 420 + 'px',
+          }}
+        >
+          <div className='card-body'>
+            <h5 className='card-title'>ROOMS BOOKED : {rooms.Count} </h5>
+            <img
+              className='card-img-top'
+              src='https://source.unsplash.com/1x1?hotel,rooms'
+              alt='Card image cap'
+            />
+            {rooms.Items !== undefined && rooms.Items.length > 0 ? (
+              <>
+                <p className='card-title'>
+                  NUMBER : <nbsp />
+                  {rooms.Items[rooms.Count - 1].number.S} , ...
+                </p>
 
-                        <p className="card-title">TYPE : <nbsp />
-                            {rooms.Items[rooms.Count-1].type.S} , ...
-                        </p>
+                <p className='card-title'>
+                  TYPE : <nbsp />
+                  {rooms.Items[rooms.Count - 1].type.S} , ...
+                </p>
 
-                        <p className="card-title">COST : <nbsp />
-                            {rooms.Items[rooms.Count-1].cost.S} , ...
-                        </p>
-
-                        </>
-                        
-                    ) : (<div />)}
-                </div>
-            </div>
-            <div className="card container" style={{ width: 18 + 'rem', top: 5 + 'rem', position: 'relative', left: 10 + 'px', top: -280 + 'px' }}>
-                <div className="card-body">
-                    <h5 className="card-title">KITCHEN ORDERS : {food.Count}</h5>
-                    <img className="card-img-top" src="https://source.unsplash.com/1x1?food" alt="Card image cap" />
-                    {food.Items != undefined && food.Items.length>0 ? (
-                        <>
-                        <p className="card-title">DISHES : <nbsp />
-                            {food.Items[food.Count-1].dish.S} , ...
-                        </p>
-
-                        <p className="card-title">COST : <nbsp />
-                            {food.Items[food.Count-1].cost.S} , ...
-                        </p>
-
-                        <p className="card-title">QUANTITY : <nbsp />
-                            {food.Items[food.Count-1].quantity.S} , ...
-                        </p>
-
-                        </>
-                        
-                    ) : (<div />)}
-                </div>
-            </div>
-            <div style={{
-                position: 'absolute',
-                right: 5,
-                top: 200,
-          }} >
-            <Chatbot/>
-            </div>
+                <p className='card-title'>
+                  COST : <nbsp />
+                  {rooms.Items[rooms.Count - 1].cost.S} , ...
+                </p>
+              </>
+            ) : (
+              <div />
+            )}
+          </div>
         </div>
-    );
-};
+        <div
+          className='card container'
+          style={{
+            width: 18 + 'rem',
+            top: 5 + 'rem',
+            position: 'relative',
+            left: 10 + 'px',
+            top: -280 + 'px',
+          }}
+        >
+          <div className='card-body'>
+            <h5 className='card-title'>KITCHEN ORDERS : {food.Count}</h5>
+            <img
+              className='card-img-top'
+              src='https://source.unsplash.com/1x1?food'
+              alt='Card image cap'
+            />
+            {food.Items != undefined && food.Items.length > 0 ? (
+              <>
+                <p className='card-title'>
+                  DISHES : <nbsp />
+                  {food.Items[food.Count - 1].dish.S} , ...
+                </p>
 
-export default Orders;
+                <p className='card-title'>
+                  COST : <nbsp />
+                  {food.Items[food.Count - 1].cost.S} , ...
+                </p>
+
+                <p className='card-title'>
+                  QUANTITY : <nbsp />
+                  {food.Items[food.Count - 1].quantity.S} , ...
+                </p>
+              </>
+            ) : (
+              <div />
+            )}
+          </div>
+        </div>
+        <div
+          style={{
+            position: 'absolute',
+            right: 5,
+            top: 200,
+          }}
+        >
+          <Chatbot />
+        </div>
+      </div>
+    )
+  }
+  return <NotFound />
+}
+
+export default Orders
--- src/pages/caesarCipher.jsx
+++ src/pages/caesarCipher.jsx
@@ -1,129 +1,158 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as Yup from "yup";
-import { Helmet } from "react-helmet";
-import { toast } from "react-toastify";
-import { Auth } from "aws-amplify";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as Yup from 'yup'
+import { Helmet } from 'react-helmet'
+import { toast } from 'react-toastify'
+import { Auth } from 'aws-amplify'
+import { useNavigate } from 'react-router-dom'
 
 const CaesarCipher = (props) => {
-  let navigate = useNavigate();
-  const [text, setText] = React.useState("Hello World!");
-  const secretKey = props.auth.user.attributes["custom:customer"]
-    ? parseInt(props.auth.user.attributes["custom:customer"]) % 26
-    : 0;
+  let navigate = useNavigate()
+  const [text, setText] = React.useState('Hello World!')
+  const secretKey = props.auth.user.attributes['custom:customer']
+    ? parseInt(props.auth.user.attributes['custom:customer']) % 26
+    : 0
 
   const validationSchema = Yup.object().shape({
-    encryptText: Yup.string().required("Provide a valid encryption text"),
-  });
+    encryptText: Yup.string().required('Provide a valid encryption text'),
+  })
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(validationSchema),
-  });
+  })
 
   function makeid(length) {
-    var result = "";
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    var charactersLength = characters.length;
+    var result = ''
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    var charactersLength = characters.length
     for (var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
     }
-    return result;
+    return result
   }
 
   React.useEffect(() => {
-    setText(makeid(5));
-  }, []);
+    setText(makeid(5))
+  }, [])
 
-  const { errors } = formState;
+  const { errors } = formState
   async function onSubmit({ encryptText }) {
-    console.log(encryptText);
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+    console.log(encryptText)
+    var myHeaders = new Headers()
+    myHeaders.append('Content-Type', 'application/json')
 
     var raw = JSON.stringify({
       cipherText: encryptText,
       key: secretKey,
       plainText: text,
-    });
-    console.log(raw);
+    })
+    console.log(raw)
 
     var requestOptions = {
-      method: "POST",
+      method: 'POST',
       headers: myHeaders,
       body: raw,
-      redirect: "follow",
-    };
+      redirect: 'follow',
+    }
 
     fetch(
-      "https://us-central1-serverlessbnb.cloudfunctions.net/Auth/doDecryption",
+      'https://us-central1-serverlessbnb.cloudfunctions.net/Auth/doDecryption',
       requestOptions
     )
       .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
+      .then(async (result) => {
+        console.log(result)
         if (result.success) {
-          props.auth.setAuthStatus(true);
-          toast.success(result.message);
-          navigate("/home");
+          props.auth.setAuthStatus(true)
+          await updateLoginStatus()
+          toast.success(result.message)
+          navigate('/home')
         } else {
-          toast.error("Invalid encrypted text!! Please try again");
+          toast.error('Invalid encrypted text!! Please try again')
         }
       })
-      .catch((error) => console.log("error", error));
+      .catch((error) => console.log('error', error))
   }
+
+  const updateLoginStatus = () => {
+    return new Promise((resolve, reject) => {
+      var myHeaders = new Headers()
+      myHeaders.append('Content-Type', 'application/json')
+
+      var raw = JSON.stringify({
+        userName: props.auth.user.username,
+        type: 'login',
+      })
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow',
+      }
+
+      fetch(
+        'https://b7tdajffli4prwkltuyqgygmr40yifbu.lambda-url.us-east-1.on.aws/',
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => resolve(result))
+        .catch((error) => console.log('error', error))
+    })
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className='flex flex-col items-center justify-center'>
       <Helmet>
         <title>B&B | 3MFA</title>
       </Helmet>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid grid-cols-1 gap-2 w-full lg:w-96">
-          <p className="text-2xl text-center mb-5 text-black font-bold">
+        <div className='grid grid-cols-1 gap-2 w-full lg:w-96'>
+          <p className='text-2xl text-center mb-5 text-black font-bold'>
             Caser Cipher
           </p>
 
-          <p className="text-2xl text-center text-black font-bold">
+          <p className='text-2xl text-center text-black font-bold'>
             Plain Text: {text}
           </p>
 
-          <p className="text-2xl text-center mb-5 text-black font-bold">
+          <p className='text-2xl text-center mb-5 text-black font-bold'>
             Customer Number: &nbsp;
             {secretKey}
           </p>
 
-          <label htmlFor="encryptText" className="text-black font-bold text-xl">
+          <label htmlFor='encryptText' className='text-black font-bold text-xl'>
             Encrypted text
           </label>
           <input
-            className="bg-gray-50 outline-none p-2 rounded-lg border-2 border-gray-600"
-            type="encrypt text"
-            name="encryptText"
-            id="encryptText"
-            placeholder="Enter caesar cipher encrypted text"
-            {...register("encryptText")}
+            className='bg-gray-50 outline-none p-2 rounded-lg border-2 border-gray-600'
+            type='encrypt text'
+            name='encryptText'
+            id='encryptText'
+            placeholder='Enter caesar cipher encrypted text'
+            {...register('encryptText')}
           />
-          <p className="text-red-600">{errors.encryptText?.message}</p>
+          <p className='text-red-600'>{errors.encryptText?.message}</p>
           <button
-            className="border-gray-600 text-black border-2 rounded-md font-bold text-xl hover:bg-gray-600 hover:text-white p-2 mt-4"
-            type="submit"
+            className='border-gray-600 text-black border-2 rounded-md font-bold text-xl hover:bg-gray-600 hover:text-white p-2 mt-4'
+            type='submit'
           >
             Verify
           </button>
-          <p className="text-center text-black mt-3 font-bold">
+          <p className='text-center text-black mt-3 font-bold'>
             Use link to decrypt: <br />
           </p>
           <a
-            className="text-center"
-            target="_blank"
-            href="https://www.dcode.fr/caesar-cipher"
+            className='text-center'
+            target='_blank'
+            href='https://www.dcode.fr/caesar-cipher'
           >
             https://www.dcode.fr/caesar-cipher
           </a>
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default CaesarCipher;
+export default CaesarCipher
