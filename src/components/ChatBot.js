8bfddafd18cfa44c3b5b309d484eb338369abd27
Prit Ajaykumar Sorathiya <pr669751@dal.ca>
2022-07-18 16:12:29 -0300

    Update session management code

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -1,34 +1,38 @@
-import React, { Component } from "react";
-import { AmplifyChatbot } from "@aws-amplify/ui-react/legacy";
-import Amplify from "aws-amplify"
+import React, { Component } from 'react'
+import { AmplifyChatbot } from '@aws-amplify/ui-react/legacy'
+import Amplify from 'aws-amplify'
 
 Amplify.configure({
   aws_project_region: 'us-east-1',
   Auth: {
-    identityPoolId: "us-east-1:f2220f75-da92-4f98-8126-fa44b8d42f3b",
-    region: 'us-east-1'
+    identityPoolId: 'us-east-1:f2220f75-da92-4f98-8126-fa44b8d42f3b',
+    region: 'us-east-1',
   },
   bots: {
-    "BookTrip_dev": {
-      "name": "BookTrip_dev",
-      "alias": "$LATEST",
-      "region": "us-east-1",
+    BookTrip_dev: {
+      name: 'BookTrip_dev',
+      alias: '$LATEST',
+      region: 'us-east-1',
     },
-  }
-
-});
+  },
+})
 
 const ChatBot = () => {
-  const msg='Hi '+localStorage.getItem('userId')+'! How can I help you?'
+  var msg
+  if (!localStorage.getItem('userId')) {
+    msg = 'Hi! How can I help you?'
+  } else {
+    msg = 'Hi ' + localStorage.getItem('userId') + '! How can I help you?'
+  }
   return (
     <div>
       <AmplifyChatbot
-        title="ServerlessBB"
-        botName="BookTrip_dev"
+        title='ServerlessBB'
+        botName='BookTrip_dev'
         welcomeMessage={msg}
       />
     </div>
-  );
-};
+  )
+}
 
-export default ChatBot;
+export default ChatBot
